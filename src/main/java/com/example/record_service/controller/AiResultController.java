a17dcaf9e061195c444650f15e3ef8d584b83ee0
zyerin <zyerin5974@gmail.com>
2024-12-12 11:55:58 +0900

    feat: AiResultController 코드 추가 - Redis에서 deviceToken 조회/FcmRequestDto 생성 후 FCM 메시지 전송

--- src/main/java/com/example/record_service/controller/AiResultController.java
+++ src/main/java/com/example/record_service/controller/AiResultController.java
@@ -1,7 +1,10 @@
 package com.example.record_service.controller;
 
 import com.example.record_service.dto.AiResultDto;
+import com.example.record_service.dto.FcmRequestDto;
 import com.example.record_service.entity.Record;
+import com.example.record_service.repository.UserServiceClient;
+import com.example.record_service.service.FcmRequestService;
 import com.example.record_service.service.RecordService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -23,6 +26,8 @@ public class AiResultController {
     private final SimpMessagingTemplate messagingTemplate;
     private final RecordService recordService;
     private static final Logger log = LoggerFactory.getLogger(AiResultController.class);
+    private final UserServiceClient userServiceClient;
+    private final FcmRequestService fcmRequestService;
 
 
     // Ai 모델에게서 해당 HTTP 요청을 보내서 결과값 받아옴
@@ -43,12 +48,37 @@ public class AiResultController {
             Optional<Record> optionalRecord = recordService.getRecordByRecordIdx(recordIdx);
             String userIdx = optionalRecord.get().getUserIdx();
 
+            // RedisMember 에서 userIdx 로 deviceToken 조회
+            String deviceToken = userServiceClient.getMemberByUserIdx(userIdx).getBody().getDeviceToken();
+            log.info("User Device Token: {}", deviceToken);
+            if (deviceToken == null || deviceToken.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User DeviceToken is not found");
+            }
+
+            // FcmRequestDto 생성
+            FcmRequestDto fcmRequest = new FcmRequestDto(
+                    deviceToken,
+                    "AI Result Notification",
+                    "Record ID: " + recordIdx + ", Result: " + result +", Text: " + text
+            );
+            log.info("FCM Request: {}", fcmRequest);
+
+            // FCM 메시지 전송
+            // String fcmResponse = fcmRequestService.sendMessage(deviceToken, resultDto);
+            String fcmResponse = fcmRequestService.sendMessage(fcmRequest);
+            log.info("FCM Response: {}", fcmResponse);
+
+            return ResponseEntity.ok("FCM message sent successfully.");
+
+            /*
             // WebSocket 경로로 메시지 전송
             String destination = "/socket/" + userIdx;
             messagingTemplate.convertAndSend(destination, result);
 
             log.info("WebSocket message sent to {}: {}", destination, result);
             return ResponseEntity.ok("Message sent to WebSocket");
+
+             */
         } catch (RuntimeException e) {
             log.error("Error: {}", e.getMessage());
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
