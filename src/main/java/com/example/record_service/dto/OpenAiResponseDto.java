6c42ba85192badd2904df99cd699ab54630865f0
zyerin <zyerin5974@gmail.com>
2024-12-13 13:30:58 +0900

    feat: OpenAiResponseDto 생성 - OpenAI API 응답을 처리

--- src/main/java/com/example/record_service/dto/OpenAiResponseDto.java
+++ src/main/java/com/example/record_service/dto/OpenAiResponseDto.java
@@ -1,2 +1,24 @@
-package com.example.record_service.dto;public class OpenAiResponseDto {
+package com.example.record_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OpenAiResponseDto {
+    private List<Choice> choices;
+
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Choice {
+        private int index;
+        private OpenAiMessageDto message;
+
+    }
 }
