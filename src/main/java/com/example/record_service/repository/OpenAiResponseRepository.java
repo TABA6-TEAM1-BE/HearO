688cd83cb31f2e844d58b08b7f45efbc0f107edc
zyerin <zyerin5974@gmail.com>
2024-12-13 13:09:38 +0900

    feat: OpenAiResponseRepository 생성

--- src/main/java/com/example/record_service/repository/OpenAiResponseRepository.java
+++ src/main/java/com/example/record_service/repository/OpenAiResponseRepository.java
@@ -1,2 +1,9 @@
-package com.example.record_service.repository;public class OpenAiResponseRepository {
+package com.example.record_service.repository;
+
+import com.example.record_service.entity.OpenAiResult;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OpenAiResponseRepository extends MongoRepository<OpenAiResult, String> {
 }
