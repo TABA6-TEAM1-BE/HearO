b384c4bc352fd960fe7bd2dcf79b3c3e6eb69529
zyerin <zyerin5974@gmail.com>
2024-12-13 09:48:01 +0900

    feat: FcmRequestDto 수정 - 중첩 클래스 Body로 AiResultDto 포함시킴

--- src/main/java/com/example/record_service/dto/FcmRequestDto.java
+++ src/main/java/com/example/record_service/dto/FcmRequestDto.java
@@ -1,16 +1,27 @@
 package com.example.record_service.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class FcmRequestDto {
     private String fcmToken;
-
     private String title;
+    private Body body;          // 중첩 클래스 Body 로 AiResultDto 를 포함시킴
 
-    private String body;
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Body {
+        private String recordIdx;
+        private String result;
+        private Boolean isHuman;
+        private String text;
+    }
 }
