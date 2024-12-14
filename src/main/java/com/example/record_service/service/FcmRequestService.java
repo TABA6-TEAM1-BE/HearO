4e4c6995651ddbf38fae06f98f5911565617596a
zyerin <zyerin5974@gmail.com>
2024-12-14 12:15:58 +0900

    refact: FcmRequestService - sendMessage에서 FirebaseMessagingException, JsonProcessingException 예외 던지도록 수정

--- src/main/java/com/example/record_service/service/FcmRequestService.java
+++ src/main/java/com/example/record_service/service/FcmRequestService.java
@@ -13,39 +13,31 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FcmRequestService {
-    private final UserServiceClient userServiceClient;
     private final ObjectMapper objectMapper;
 
-    public String sendMessage(FcmRequestDto requestDto) {
-        try {
-            // FcmRequestDto.Body -> json 으로 변환
-            String jsonBody = objectMapper.writeValueAsString(requestDto.getBody());
-            log.info("Before: {}", requestDto.getBody());
-            log.info("FcmRequest Body: {}", jsonBody);
+    public String sendMessage(FcmRequestDto requestDto) throws JsonProcessingException, FirebaseMessagingException {
+        // FcmRequestDto.Body -> json으로 변환
+        String jsonBody = objectMapper.writeValueAsString(requestDto.getBody());
+        log.info("Before: {}", requestDto.getBody());
+        log.info("FcmRequest Body: {}", jsonBody);
 
-            // FCM 메시지 생성
-            Message message = Message.builder()
-                    .setToken(requestDto.getFcmToken())
-                    .putData("title", requestDto.getTitle())
-                    .putData("body", jsonBody) // json 형식으로 전송
-                    .build();
-            log.info("Message: {}", message);
+        // FCM 메시지 생성
+        Message message = Message.builder()
+                .setToken(requestDto.getFcmToken())
+                .putData("title", requestDto.getTitle())
+                .putData("body", jsonBody) // json 형식으로 전송
+                .build();
+        log.info("Message: {}", message);
 
-            // 메시지 전송
-            String response = FirebaseMessaging.getInstance().send(message);
-            log.info("Message sent successfully. FCM response: {}", response);
+        // 메시지 전송
+        String response = FirebaseMessaging.getInstance().send(message);
+        log.info("Message sent successfully. FCM response: {}", response);
 
-            return response;
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-            return "Failed to process JSON: " + e.getMessage();
-        }catch (FirebaseMessagingException e) {
-            log.error("Error sending FCM message: {}", e.getMessage(), e);
-            return "Failed to send FCM message: " + e.getMessage();
-        }
+        return response;
     }
 }
+
