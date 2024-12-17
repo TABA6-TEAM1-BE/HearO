428c5c567a7a70d2676c6f43263c8c7befbf3f09
kimjungho <kwjdgh1267@gmail.com>
2024-12-17 18:07:19 +0900

    Feat: ai promt changed and multi sound processing added

--- src/main/java/com/example/record_service/controller/AiResultController.java
+++ src/main/java/com/example/record_service/controller/AiResultController.java
@@ -3,7 +3,9 @@ package com.example.record_service.controller;
 import com.example.record_service.dto.AiResultDto;
 import com.example.record_service.dto.FcmRequestDto;
 import com.example.record_service.entity.Record;
+import com.example.record_service.repository.RecordRepository;
 import com.example.record_service.repository.UserServiceClient;
+import com.example.record_service.service.CounterService;
 import com.example.record_service.service.FcmRequestService;
 import com.example.record_service.service.OpenAiService;
 import com.example.record_service.service.RecordService;
@@ -21,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
 
@@ -34,6 +37,8 @@ public class AiResultController {
     private final UserServiceClient userServiceClient;
     private final FcmRequestService fcmRequestService;
     private final OpenAiService openAiService;
+    private final CounterService counterService;
+    private final RecordRepository recordRepository;
 
     // Ai 모델에게서 해당 HTTP 요청을 보내서 결과값 받아옴
     @PostMapping("/results")
@@ -45,8 +50,8 @@ public class AiResultController {
             Boolean isHuman = resultDto.getIsHuman();
             String text = isHuman ? resultDto.getText() : null; // isHuman일 경우에만 text 값 저장
 
-            String prompt = "이 문장에서 틀린 맞춤법과 띄어쓰기를 올바르게 고쳐주고 문맥이 자연스럽게 수정해줘. " +
-                    "그리고 출력은 내가 제공한 해당 문장만 고쳐서 나오게 해주고 앞 뒤에 다른 추가 설명은 필요없어. : ";
+            String prompt = "이 문장에서 중요 내용만 정리해서 요약해줘.  " +
+                    "앞 뒤에 다른 추가 설명은 필요없어. : ";
 
             // OpenAI 후처리 (isHuman 인 경우)
             if (isHuman && text != null && !text.isEmpty()) {
@@ -59,40 +64,121 @@ public class AiResultController {
                 }
             }
 
-            // Record 업데이트
-            if (!recordService.updateRecordWithAIResult(recordIdx, result, isHuman, text)) {
-                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Record with recordIdx " + recordIdx + " not found.");
+            if(result.contains(",")){
+                String [] resultArr = result.split(",");
+                // Record 업데이트
+                if (!recordService.updateRecordWithAIResult(recordIdx, resultArr[0], false, null)) {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Record with recordIdx " + recordIdx + " not found.");
+                }
+
+
+                Optional<Record> optionalRecord = recordService.getRecordByRecordIdx(recordIdx);
+                String userIdx = optionalRecord.get().getUserIdx();
+
+                //새로 레코드 추가
+                // 자동 증가 recordIdx 생성
+                long newRecordIdx = counterService.getNextRecordIdxSequence("record_idx");
+                String newrecordIdx = String.valueOf(newRecordIdx);
+                // 현재 시간 저장
+                LocalDateTime time = LocalDateTime.now();
+                // Record 생성 후 저장
+                Record record = new Record(userIdx,resultArr[1],time);
+                record.setRecordIdx(String.valueOf(newrecordIdx)); // 자동 생성된 recordIdx
+                record.setText(text);
+                recordRepository.save(record);
+
+
+
+
+                // RedisMember 에서 userIdx 로 deviceToken 조회
+                String deviceToken = userServiceClient.getMemberByUserIdx(userIdx).getBody().getFcmToken();
+                log.info("User Device Token: {}", deviceToken);
+
+                // FcmRequestDto 생성
+                FcmRequestDto fcmRequest = FcmRequestDto.builder()
+                        .fcmToken(deviceToken)
+                        .notification(FcmRequestDto.Notification.builder()
+                                .title("우리 앱 이름")
+                                .body(result + "에 대한 알림이 도착했습니다!")
+                                .build())
+                        .data(FcmRequestDto.Data.builder()
+                                .recordIdx(recordIdx)
+                                .result(resultArr[0])
+                                .isHuman(false)
+                                .text(null)
+                                .build())
+                        .build();
+                log.info("FCM Request: {}", fcmRequest);
+
+                // FCM 메시지 전송
+                // String fcmResponse = fcmRequestService.sendMessage(deviceToken, resultDto);
+                String fcmResponse = fcmRequestService.sendMessage(fcmRequest);
+                log.info("FCM Response: {}", fcmResponse);
+
+
+                // FcmRequestDto2 생성
+                FcmRequestDto fcmRequest2 = FcmRequestDto.builder()
+                        .fcmToken(deviceToken)
+                        .notification(FcmRequestDto.Notification.builder()
+                                .title("우리 앱 이름")
+                                .body(result + "에 대한 알림이 도착했습니다!")
+                                .build())
+                        .data(FcmRequestDto.Data.builder()
+                                .recordIdx(newrecordIdx)
+                                .result(resultArr[1])
+                                .isHuman(isHuman)
+                                .text(text)
+                                .build())
+                        .build();
+                log.info("FCM Request: {}", fcmRequest2);
+
+                // FCM 메시지 전송
+                // String fcmResponse2 = fcmRequestService.sendMessage(deviceToken, resultDto);
+                String fcmResponse2 = fcmRequestService.sendMessage(fcmRequest2);
+                log.info("FCM Response: {}", fcmResponse2);
+
+
+                return ResponseEntity.ok("FCM message sent successfully.");
+
+
+            }else{
+                // Record 업데이트
+                if (!recordService.updateRecordWithAIResult(recordIdx, result, isHuman, text)) {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Record with recordIdx " + recordIdx + " not found.");
+                }
+
+                Optional<Record> optionalRecord = recordService.getRecordByRecordIdx(recordIdx);
+                String userIdx = optionalRecord.get().getUserIdx();
+
+                // RedisMember 에서 userIdx 로 deviceToken 조회
+                String deviceToken = userServiceClient.getMemberByUserIdx(userIdx).getBody().getFcmToken();
+                log.info("User Device Token: {}", deviceToken);
+
+                // FcmRequestDto 생성
+                FcmRequestDto fcmRequest = FcmRequestDto.builder()
+                        .fcmToken(deviceToken)
+                        .notification(FcmRequestDto.Notification.builder()
+                                .title("우리 앱 이름")
+                                .body(result + "에 대한 알림이 도착했습니다!")
+                                .build())
+                        .data(FcmRequestDto.Data.builder()
+                                .recordIdx(recordIdx)
+                                .result(result)
+                                .isHuman(isHuman)
+                                .text(text)
+                                .build())
+                        .build();
+                log.info("FCM Request: {}", fcmRequest);
+
+                // FCM 메시지 전송
+                // String fcmResponse = fcmRequestService.sendMessage(deviceToken, resultDto);
+                String fcmResponse = fcmRequestService.sendMessage(fcmRequest);
+                log.info("FCM Response: {}", fcmResponse);
+
+                return ResponseEntity.ok("FCM message sent successfully.");
             }
 
-            Optional<Record> optionalRecord = recordService.getRecordByRecordIdx(recordIdx);
-            String userIdx = optionalRecord.get().getUserIdx();
 
-            // RedisMember 에서 userIdx 로 deviceToken 조회
-            String deviceToken = userServiceClient.getMemberByUserIdx(userIdx).getBody().getFcmToken();
-            log.info("User Device Token: {}", deviceToken);
-
-            // FcmRequestDto 생성
-            FcmRequestDto fcmRequest = FcmRequestDto.builder()
-                    .fcmToken(deviceToken)
-                    .notification(FcmRequestDto.Notification.builder()
-                            .title("우리 앱 이름")
-                            .body(result + "에 대한 알림이 도착했습니다!")
-                            .build())
-                    .data(FcmRequestDto.Data.builder()
-                            .recordIdx(recordIdx)
-                            .result(result)
-                            .isHuman(isHuman)
-                            .text(text)
-                            .build())
-                    .build();
-            log.info("FCM Request: {}", fcmRequest);
-
-            // FCM 메시지 전송
-            // String fcmResponse = fcmRequestService.sendMessage(deviceToken, resultDto);
-            String fcmResponse = fcmRequestService.sendMessage(fcmRequest);
-            log.info("FCM Response: {}", fcmResponse);
-
-            return ResponseEntity.ok("FCM message sent successfully.");
 
             /*
             // WebSocket 경로로 메시지 전송
@@ -117,4 +203,8 @@ public class AiResultController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send Fcm message");
         }
     }
+
 }
+
+
+
